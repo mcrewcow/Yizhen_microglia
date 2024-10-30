8487d8b900ac9ec07f2fc7883a2642e9809b553a
Emil Kriukov <77118598+mcrewcow@users.noreply.github.com>
2024-10-30 13:38:08 -0400

    Create metabolism_genes.R

---
+++ metabolism_genes.R
@@ -0,0 +1,16 @@
+#Load adult human Seurat object
+library(reshape2)
+library(tidyr)
+
+avg_expr <- AverageExpression(adult, features = c('IDO1','IDO2','KYNU','KMO','QPRT','KYAT','LAT1'))
+avg_expr <- AverageExpression(adult, features = c('SLC7A5'))
+avg_expr_matrix <- avg_expr$RNA
+avg_expr_long <- melt(as.matrix(avg_expr_matrix))
+colnames(avg_expr_long) <- c("Gene", "Group", "Expression")
+
+ggplot(avg_expr_long, aes(x = Group, y = Gene, fill = Expression)) +
+geom_tile() +
+scale_fill_gradientn(colours = c("blue", "white", "red")) + theme_minimal() +
+# Custom color gradient
+theme(axis.text.x = element_text(angle = 90, hjust = 1)) + # Adjust x-axis text
+labs(x = "Group", y = "Gene", fill = "Expression") + coord_fixed()
